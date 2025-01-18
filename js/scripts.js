3ceecde611cc11192644c6113f94804369a71f06
kuma0304 <kuma0304@algonquinlive.com>
2025-01-17 19:01:28 -0500

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// Task 1
+let a = 3;
+let b = 5;
+let c;
+
+const taskA = `let a = 3;
+let b = 5;
+let c;
+-------------
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}`;
+console.log(taskA);
+
+
+
+// Task 2
+let first_name = "Robin";
+let last_name = "Kumar";
+let email = "kuma0304@algonquinlive.com";
+let output;
+
+output = "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ".";
+console.log(output);
